747d39b62a0ff825296e635598a7a80f94985b70
abijarchi <abijarchi@learn.senecac.on.ca>
2012-01-28 11:59:38 -0500

    'supply model updated'

--- application/models/supply.php
+++ application/models/supply.php
@@ -5,7 +5,16 @@ class Supply extends DataMapper {
 	var $has_many = array('supplyitem');
 	var $table = 'supplies';
 
-
+	var $validation = array(
+		'description' => array(
+	    	'label' => 'Supply Description',
+	    	'rules' => array('required','trim')
+		),
+		'price' => array(
+	    	'label' => 'Supply Price',
+	    	'rules' => array('required','trim')
+		),
+	);
     function __construct($id = NULL)
     {
         parent::__construct();
